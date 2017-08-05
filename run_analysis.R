9986e52882d10b1d04d2f63986b23e23bc3adcab
Christian Karcher <contact@ckarcher.de>
2017-08-05 15:20:04 +0200

    final commented version analysis

--- run_analysis.R
+++ run_analysis.R
@@ -12,21 +12,21 @@ fileTrainY <- file.path("UCI HAR Dataset", "train",  "y_train.txt")
 fileTestY <- file.path("UCI HAR Dataset", "test",  "y_test.txt")
 fileTidy <- file.path("tidyData.txt")
 
-# get features and test/train data, labeled accordingly
-
-
+# get features and test/train data, label accordingly & merge
 
 featureNames <- read.csv(fileFeatures, sep=";", header=F)$V1
 
-trainData <- read_fwf(fileTrainX, fwf_widths(rep(16, length(featureNames)), col_names = as.character(featureNames)))
+trainData <- read_fwf(fileTrainX, fwf_widths(rep(16, length(featureNames)), col_names = as.character(featureNames)), cols(.default = col_double()))
 
-testData <- read_fwf(fileTestX, fwf_widths(rep(16, length(featureNames)), col_names = as.character(featureNames)))
+testData <- read_fwf(fileTestX, fwf_widths(rep(16, length(featureNames)), col_names = as.character(featureNames)), cols(.default = col_double()))
 
 fullData <- rbind(testData, trainData)
 
+# filter data bei mean() and std() entries only
 
 filterData <- as.data.frame(fullData[,grepl("(mean\\(\\)|std\\(\\))$", featureNames)])
 
+# get activity labels 
 activityLabels <- read.csv(fileActivity, sep=" ", header=F)$V2
 
 trainLabels = read.csv(fileTrainY, sep=";", header=F)$V1
@@ -34,7 +34,11 @@ testLabels = read.csv(fileTestY, sep=";", header=F)$V1
 fullLabels <- c(testLabels, trainLabels)
 activity <- activityLabels[fullLabels]
 
+# merge activities with remaining data
+
 filterData <- cbind(activity,filterData)
+
+# summarise filtered data by activity, write to file
 
 filterData %>%
   group_by(activity) %>%
